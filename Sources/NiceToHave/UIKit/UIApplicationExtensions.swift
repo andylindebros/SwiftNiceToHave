5f289cb338ee4729144fc75cdbe3bdaf0ee36fc7
Andreas Linde <andylindebros@gmail.com>
2023-11-02 12:50:41 +0100

    Added UIApplocationWrapper

--- Sources/NiceToHave/UIKit/UIApplicationExtensions.swift
+++ Sources/NiceToHave/UIKit/UIApplicationExtensions.swift
@@ -1,66 +1,83 @@
 import Foundation
 
 #if os(iOS)
-import UIKit
+    import UIKit
 #endif
 #if canImport(UIKit)
-public protocol UIApplicationProvider {
-    func canOpenURL(_ url: URL) -> Bool
-    func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any], completionHandler: ((Bool) -> Void)?)
-    @discardableResult @MainActor func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any]) async -> Bool
-    @MainActor var isIdleTimerDisabled: Bool { get set }
-}
-
-public extension UIApplicationProvider {
-    func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any] = [:], completionHandler: ((Bool) -> Void)?) {
-        open(url, options: options, completionHandler: completionHandler)
+    public protocol UIApplicationProvider {
+        @MainActor func canOpenURL(_ url: URL) -> Bool
+        func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any], completionHandler: ((Bool) -> Void)?)
+        @discardableResult @MainActor func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any]) async -> Bool
+        @MainActor var isIdleTimerDisabled: Bool { get set }
     }
 
-    @discardableResult @MainActor func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any] = [:]) async -> Bool {
-        return await open(url, options: options)
-    }
-}
+    public extension UIApplicationProvider {
+        func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any] = [:], completionHandler: ((Bool) -> Void)?) {
+            open(url, options: options, completionHandler: completionHandler)
+        }
 
-extension UIApplication: UIApplicationProvider {}
-
-public final class UIApplicationMock: UIApplicationProvider {
-    public init(isIdleTimerDisabled: Bool = false) {
-        self.isIdleTimerDisabled = isIdleTimerDisabled
+        @discardableResult @MainActor func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any] = [:]) async -> Bool {
+            return await open(url, options: options)
+        }
     }
 
-    public private(set) var events: [Event] = []
-    private var canOpenURLResponse = true
-    private var openResponse = true
+    // Since UIApplication.shared can return nil even though the singleton is granted, a wrapper is provided that can be used to inject as a dependency.
+    struct UIApplicationWrapper: UIApplicationProvider {
+        func canOpenURL(_ url: URL) -> Bool {
+            UIApplication.shared.canOpenURL(url)
+        }
 
-    public func canOpenURL(_ url: URL) -> Bool {
-        events.append(.canOpenURL(url))
-        return canOpenURLResponse
+        var isIdleTimerDisabled: Bool {
+            didSet {
+                UIApplication.shared.isIdleTimerDisabled = isIdleTimerDisabled
+            }
+        }
+
+        func open(_ url: URL, options _: [UIApplication.OpenExternalURLOptionsKey: Any]) async -> Bool {
+            await UIApplication.shared.open(url)
+        }
     }
 
-    public func open(url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any]) async -> Bool {
-        events.append(.open(url, options: options))
-        return openResponse
-    }
+    extension UIApplication: UIApplicationProvider {}
 
-    public var isIdleTimerDisabled: Bool
+    public final class UIApplicationMock: UIApplicationProvider {
+        public init(isIdleTimerDisabled: Bool = false) {
+            self.isIdleTimerDisabled = isIdleTimerDisabled
+        }
 
-    @discardableResult public func setCanOpenURLResponse(to value: Bool) -> Self {
-        canOpenURLResponse = value
-        return self
-    }
+        public private(set) var events: [Event] = []
+        private var canOpenURLResponse = true
+        private var openResponse = true
 
-    @discardableResult public func setOpenResponse(to value: Bool) -> Self {
-        openResponse = value
-        return self
-    }
+        public func canOpenURL(_ url: URL) -> Bool {
+            events.append(.canOpenURL(url))
+            return canOpenURLResponse
+        }
 
-    public func reset() {
-        events = []
-    }
+        public func open(url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any]) async -> Bool {
+            events.append(.open(url, options: options))
+            return openResponse
+        }
 
-    public enum Event {
-        case canOpenURL(URL)
-        case open(URL, options: [UIApplication.OpenExternalURLOptionsKey: Any])
+        public var isIdleTimerDisabled: Bool
+
+        @discardableResult public func setCanOpenURLResponse(to value: Bool) -> Self {
+            canOpenURLResponse = value
+            return self
+        }
+
+        @discardableResult public func setOpenResponse(to value: Bool) -> Self {
+            openResponse = value
+            return self
+        }
+
+        public func reset() {
+            events = []
+        }
+
+        public enum Event {
+            case canOpenURL(URL)
+            case open(URL, options: [UIApplication.OpenExternalURLOptionsKey: Any])
+        }
     }
-}
 #endif
