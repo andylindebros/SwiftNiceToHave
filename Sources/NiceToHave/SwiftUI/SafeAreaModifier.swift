b2e1145796c5bd303a92de1e10f3452052922a40
Andreas Linde <andylindebros@gmail.com>
2024-01-09 09:03:23 +0100

    Renamed safeareapadding
    Renamed safeareapadding since it is conflicting with SwiftUIs
    safeAreaPadding

--- Sources/NiceToHave/SwiftUI/SafeAreaModifier.swift
+++ Sources/NiceToHave/SwiftUI/SafeAreaModifier.swift
@@ -3,13 +3,14 @@ import SwiftUI
 
 #if canImport(UIKit)
     public extension View {
-        @MainActor func safeAreaPadding() -> some View {
-            modifier(SafeAreaPaddingModifier())
+        @MainActor func paddingSafeArea(_ insets: Edge.Set = .all) -> some View {
+            modifier(SafeAreaPaddingModifier(insets: insets))
         }
     }
 
     private struct SafeAreaPaddingModifier: ViewModifier {
-        @MainActor init() {
+        @MainActor init(insets: Edge.Set) {
+            self.insets = insets
             safeAreaInsets = UIApplication
                 .shared
                 .connectedScenes
@@ -17,12 +18,23 @@ import SwiftUI
                 .first { $0.isKeyWindow }?
                 .safeAreaInsets ?? .zero
         }
-
+        let insets: Edge.Set
         let safeAreaInsets: UIEdgeInsets
 
         func body(content: Content) -> some View {
             content
-                .padding(.top, safeAreaInsets.top)
+                .if(insets.contains(.top)) { view in
+                    view.padding(.top, safeAreaInsets.top)
+                }
+                .if(insets.contains(.bottom)) { view in
+                    view.padding(.bottom, safeAreaInsets.bottom)
+                }
+                .if(insets.contains(.leading)) { view in
+                    view.padding(.leading, safeAreaInsets.left)
+                }
+                .if(insets.contains(.trailing)) { view in
+                    view.padding(.trailing, safeAreaInsets.right)
+                }
                 .padding(.bottom, safeAreaInsets.bottom)
         }
     }
