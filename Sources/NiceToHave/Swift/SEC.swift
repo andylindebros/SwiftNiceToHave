8d17cb69843969833a13e4bb602291e4c63e767d
Andreas Linde <andylindebros@gmail.com>
2024-08-13 02:41:22 +0200

    Added sec protocol

---
+++ Sources/NiceToHave/Swift/SEC.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+// Conform to (S)endable, (E)quatable and (C)odable
+public protocol SEC: Sendable, Equatable, Codable {}
+
+// Conform to (S)endable, (E)quatable, (C)odable and Identifiable
+public protocol SECI: SEC, Identifiable {}
