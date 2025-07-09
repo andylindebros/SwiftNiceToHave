00ab1e3c531753d28dde288b5c1c949ba7f6e1c6
Andreas Linde <andylindebros@gmail.com>
2025-07-09 07:52:14 -0400

    Changed taskManager to be an actor

--- Sources/NiceToHave/Swift/TaskManager.swift
+++ Sources/NiceToHave/Swift/TaskManager.swift
@@ -1,20 +1,20 @@
 import Foundation
 
 public protocol TaskManagerProvider {
-    func singleTask(_ task: Task<Void, Never>, withID id: String)
-    func cancelTask(withID id: String)
+    func singleTask(_ task: Task<Void, Never>, withID id: String) async
+    func cancelTask(withID id: String) async
 }
 
-public class TaskManager: TaskManagerProvider {
+public actor TaskManager: TaskManagerProvider {
     public init() {}
     private var tasks = [String: Task<Void, Never>]()
 
-    public func singleTask(_ task: Task<Void, Never>, withID id: String) {
+    public func singleTask(_ task: Task<Void, Never>, withID id: String) async {
         tasks[id]?.cancel()
         tasks[id] = task
     }
 
-    public func cancelTask(withID id: String) {
+    public func cancelTask(withID id: String) async {
         tasks[id]?.cancel()
         tasks.removeValue(forKey: id)
     }
@@ -23,7 +23,7 @@ public class TaskManager: TaskManagerProvider {
 public extension TaskManager {
     struct Mock: TaskManagerProvider {
         public init() {}
-        public func singleTask(_: Task<Void, Never>, withID _: String) {}
-        public func cancelTask(withID _: String) {}
+        public func singleTask(_: Task<Void, Never>, withID _: String) async {}
+        public func cancelTask(withID _: String) async {}
     }
 }
