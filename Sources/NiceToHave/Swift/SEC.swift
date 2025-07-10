8c5d46d89f5de33bc5f3f260c6de16b6fb81302b
Andreas Linde <andylindebros@gmail.com>
2025-07-10 07:29:59 +0200

    fix doc

--- Sources/NiceToHave/Swift/SEC.swift
+++ Sources/NiceToHave/Swift/SEC.swift
@@ -3,7 +3,7 @@ import Foundation
 // Conform to (S)endable, (E)quatable and (C)odable
 public protocol SEC: Sendable, Equatable, Codable {}
 
-// Conform to (S)endable, (E)quatable, (C)odable and Identifiable
+// Conform to (S)endable, (E)quatable, (C)odable and (I)dentifiable
 public protocol SECI: SEC, Identifiable {}
 
 public protocol SECH: Codable, Hashable, Equatable, Sendable {}
